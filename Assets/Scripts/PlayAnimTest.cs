919878353811c18204e69125b79b23d7ba9f66d7
azraelzc <460366051@qq.com>
2018-06-25 14:47:01 +0800

    测试工程

---
+++ Assets/Scripts/PlayAnimTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimTest : MonoBehaviour {
+
+    private float delayTime = 0;
+    private Animation anim;
+	// Use this for initialization
+	void Start () {
+        anim = transform.GetComponent<Animation>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        delayTime += Time.deltaTime;
+        if (delayTime > 5)
+        {
+            delayTime = 0;
+            if (anim != null)
+            {
+                Debug.Log("===play===="+anim.Play("testAnimation"));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayAnimatorTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimatorTest : MonoBehaviour {
+
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = transform.GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (animator != null)
+        {
+            AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(0);
+            Debug.Log(info.normalizedTime);
+        }
+	}
+}
