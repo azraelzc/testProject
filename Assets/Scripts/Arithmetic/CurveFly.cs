408d0874b18da946957747322cc8e1316a203429
azraelzc <460366051@qq.com>
2021-03-08 16:39:14 +0800

    change ignore

---
+++ Assets/Scripts/Arithmetic/CurveFly.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveFly : MonoBehaviour {
+    public Vector3 StartPos;
+    public Vector3 EndPos;
+    public Vector3 InitialVec;
+    public float FlyTime;
+    public GameObject obj;
+
+    Vector3 _CurrentPos;
+    Vector3 _EndPos;
+    Vector3 _CurrectVec;
+    float _Time;
+    float _TotalTime;
+    float _Speed;
+    bool isFly = false;
+    // Start is called before the first frame update
+    void Start() {
+        _CurrentPos = StartPos;
+        _EndPos = EndPos;
+        transform.position = StartPos;
+        _CurrectVec = InitialVec;
+        _Time = FlyTime;
+        _TotalTime = FlyTime;
+        isFly = true;
+
+        _Speed = Vector3.Distance(StartPos,EndPos)/_Time;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isFly) {
+            Vector3 targetVec = (_EndPos - _CurrentPos) / _Time * Time.deltaTime;
+            _CurrectVec = (_CurrectVec+ targetVec);
+            _CurrentPos = _CurrentPos + _CurrectVec;
+            //obj.transform.LookAt(_CurrectVec);
+            transform.position = _CurrentPos;
+            Debug.LogError("==Update==" + _CurrentPos + " : " + _CurrectVec + " : " + targetVec);
+            _Time -= Time.deltaTime;
+            if (_Time <= 0) {
+                //transform.position = _EndPos;
+                Debug.Log("====arrive===");
+                isFly = false;
+                //Start();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Arithmetic/GravityFlyYolk.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 重力为垂直于a、b两点，方向可以指定左右
+ */
+public class GravityFlyYolk : MonoBehaviour {
+    public float time = 3;//代表从A点出发到B经过的时长
+    public Vector3 pointA;//点A
+    public Vector3 pointB;//点B
+    public float gravity;//重力向量
+    public bool isUp;
+
+    Vector2 _StartPos;
+    Vector2 _EndPos;
+    float _Time;
+    bool _IsUp;
+
+    Vector2 _CurrentVec; //力的向量
+    Vector2 _GravityForce;  //重力向量
+    Vector2 _PointDir;
+
+    bool isFly = false;
+    void Start() {
+        _StartPos = new Vector2(pointA.x, pointA.z);
+        _EndPos = new Vector2(pointB.x, pointB.z);
+        _Time = time;
+        _IsUp = isUp;
+        transform.position = pointA;//将物体置于A点
+        _PointDir = _EndPos - _StartPos;
+        //通过一个式子计算初速度
+        _CurrentVec = _PointDir / _Time;
+        _GravityForce = GetVerticalVec() * gravity;
+        _CurrentVec = _CurrentVec - _GravityForce * _Time / 2;
+        isFly = true;
+        Debug.LogError("==_GravityForce==" + _GravityForce + " : " + Vector3.Dot(_PointDir, _GravityForce));
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isFly) {
+            float angle = GetAngle(_CurrentVec, Vector2.left);
+            _CurrentVec += _GravityForce * Time.deltaTime;
+            Vector3 targetVec = _CurrentVec * Time.deltaTime;
+            Vector3 v = new Vector3(targetVec.x, 0, targetVec.y);
+            transform.position = transform.position + v;
+            transform.localEulerAngles = new Vector3(0, angle, 0);
+            _Time -= Time.deltaTime;
+            if (_Time <= 0) {
+                Debug.Log("====arrive===");
+                isFly = false;
+                //Start();
+            }
+        }
+    }
+
+    Vector2 GetVerticalVec() {
+        if (_IsUp) {
+            return new Vector2(_CurrentVec.y, -_CurrentVec.x).normalized;
+        } else {
+            return new Vector2(-_CurrentVec.y, _CurrentVec.x).normalized;
+        }
+    }
+
+    float GetAngle(Vector2 a, Vector2 b) {
+        float angle = Vector2.Angle(a,b);
+        b.x -= a.x;
+        b.y -= a.y;
+        if (b.y > 0) {
+            angle = -angle;
+        }
+        return angle;
+    }
+}
